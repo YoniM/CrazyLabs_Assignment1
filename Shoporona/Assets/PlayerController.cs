17984ba5e2120083cff18c14112e57c5d612df56
YoniM <yoni.maor@gmail.com>
2020-07-10 01:28:30 +0300

    Added Sneezing Man
    Particle effect of sneezing
    + basic animation for forward movement of player

--- Shoporona/Assets/PlayerController.cs
+++ Shoporona/Assets/PlayerController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform player;
+    public Transform playerbody;
+    public float vel = 5f;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = player.GetComponent<Rigidbody>();
+        rb.velocity = vel * player.forward;
     }
 
     // Update is called once per frame

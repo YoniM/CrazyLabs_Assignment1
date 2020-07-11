172bea6ae198a67168f0b2a9db20311d265f094c
YoniM <yoni.maor@gmail.com>
2020-07-11 13:53:52 +0300

    Add Road and Car

---
+++ CanYouGetHome/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float v0;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //Color fullDark = new Color(32.0f / 255.0f, 28.0f / 255.0f, 46.0f / 255.0f);
+        //RenderSettings.ambientLight = fullDark;
+        rb.velocity = transform.forward * v0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

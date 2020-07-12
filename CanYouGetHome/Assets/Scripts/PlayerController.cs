0c68b81f9cd691e59145953dfb68b9e774cf7295
YoniM <yoni.maor@gmail.com>
2020-07-12 12:31:34 +0300

    Fixed steering
    + added acceleration at start of movement

--- CanYouGetHome/Assets/Scripts/PlayerController.cs
+++ CanYouGetHome/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float v0;
+    public float vmax = 18f; // [m/sec]
+    public float steering_streangth = 20f;
     private Rigidbody rb;
-    float steering_streangth = 20f;
-    float rotation_factor, force_factor;
 
+    private float vel; // [m/sec]
+    float acce; // [m/sec^2]
+
+    float rotation_factor, force_factor;
     private float moveX;
 
     // Start is called before the first frame update
@@ -17,25 +20,45 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         //Color fullDark = new Color(32.0f / 255.0f, 28.0f / 255.0f, 46.0f / 255.0f);
         //RenderSettings.ambientLight = fullDark;
-        rb.velocity = transform.forward * v0;
+        //rb.velocity = transform.forward * v0;
         rotation_factor = steering_streangth * 0.1f;
         force_factor = steering_streangth * 2.0f;
+
+        vel = 0f;
+        acce = vmax;
     }
 
     void FixedUpdate()
     {
         moveX = Input.GetAxis("Horizontal");
-        float force = force_factor * (v0 * moveX) * Time.deltaTime;
+        float force = force_factor * (vel * moveX) * Time.deltaTime;
         rb.AddForce(force * transform.right);
     }
 
     private void Update()
     {
+        if (vel < vmax)
+            AccelerateCar(acce);
+
         //moveX = Input.GetAxis("Horizontal");
-        float rotation = rotation_factor * v0 * moveX * Time.deltaTime;
+        float rotation = rotation_factor * vel * moveX * Time.deltaTime;
         transform.Rotate(0, rotation, 0);
+        transform.position += transform.forward * vel * Time.deltaTime; // moving forward at speed of vel
+        //Debug.Log(transform.rotation);
+
+        if (Input.GetKeyDown("space"))
+            StopCar();
     }
 
-    
+    private void AccelerateCar(float acceleration)
+    {
+        vel += acceleration * Time.deltaTime;
+        vel = Mathf.Min(vel, vmax);
+    }
+
+    private void StopCar()
+    {
+        vel = 0f;
+    }
 
 }

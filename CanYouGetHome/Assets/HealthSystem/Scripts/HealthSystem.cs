19b0618d0caa89c629a15457bfbe67ff18c35b02
YoniM <yoni.maor@gmail.com>
2020-07-12 18:18:43 +0300

    Some time before Unity Crashed :(

---
+++ CanYouGetHome/Assets/HealthSystem/Scripts/HealthSystem.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public static HealthSystem Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public Image crash1 , crash2 , crash3;
+    private int crashes;
+    private int maxcrashes = 3;
+
+    public int Crashes { get { return crashes; } }
+    public int MaxCrashes { get { return maxcrashes; } }
+
+private void Start()
+    {
+        crashes = 0;
+        UpdateVisual();
+    }
+
+    public void UpdateVisual()
+    {
+        crash1.enabled = crash2.enabled = crash3.enabled = false;
+        if (crashes >= 1)
+            crash1.enabled = true;
+        if (crashes >= 2)
+            crash2.enabled = true;
+        if (crashes >= 3)
+            crash3.enabled = true;
+    }
+    public void AddCrash()
+    {
+        crashes++;
+        if (crashes > maxcrashes)
+        {
+            GameOver();
+        } else
+        {
+            UpdateVisual();
+        }
+    }
+
+    public void GameOver()
+    {
+        Debug.LogError("GameOver");
+    }
+
+
+}

b9ed6a40b7fa1689c26945b9fd4fc202a95300e2
YoniM <yoni.maor@gmail.com>
2020-07-16 13:54:22 +0300

    Added to sip system

---
+++ CanYouGetHome/Assets/Scripts/SipBodyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SipBodyScript : MonoBehaviour
+{
+    public AlcoholEffect alcoholeff;
+    private void OnCollisionEnter(Collision collision)
+    {
+        BeerIconScript beer;
+        beer = collision.gameObject.GetComponent<BeerIconScript>();
+        
+        if (beer != null)
+        {
+            alcoholeff.BeerUp();
+            beer.gameObject.SetActive(false);
+        }
+    }
+}
